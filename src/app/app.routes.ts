346fe0d9f68026b0979c22c66a1cffc3212a8093
Tiago Barbosa <sombr43@gmail.com>
2026-01-04 16:38:32 -0300

    feat: add create component and  update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { CreateProductComponent } from './features/create-product/create-product.component';
 import { ListComponent } from './features/list/list.component';
 
 export const routes: Routes = [
-  { path: '', component: ListComponent }
+  { path: '', component: ListComponent },
+  { path: 'create-product', component: CreateProductComponent }
 ];
