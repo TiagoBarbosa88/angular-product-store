f697439b1f3a6361078f0e585eb228d5dd3f38a8
Tiago Barbosa <sombr43@gmail.com>
2026-01-04 16:44:04 -0300

    fix: loads createComponent via lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { ListComponent } from './features/list/list.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
-  { path: 'create-product', component: CreateProductComponent }
+  { path: 'create-product',
+     loadComponent: () => import('./features/create-product/create-product.component').then(m => m.CreateProductComponent) }
 ];
