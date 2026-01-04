0be6a3c16e714596df49e97daad2dcc912f6127a
Tiago Barbosa <sombr43@gmail.com>
2026-01-04 14:22:53 -0300

    feat: List Products with template

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../../models/products';
+import { ProductServiceService } from './../../services/product-service.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  products$: Products[] = []
+
+  constructor(private productService: ProductServiceService){}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe( products  => {
+      this.products$ = products
+
+      console.log(this.products$)
+    })
+  }
 
 }
