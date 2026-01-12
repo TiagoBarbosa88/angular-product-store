4bee83cd58a9162d6918230e23e592167bbbd601
TiagoBarbosa88 <sombr43@gmail.com>
2026-01-12 16:33:57 -0300

    feat: Update createComponent

--- src/app/features/create-product/create-product.component.ts
+++ src/app/features/create-product/create-product.component.ts
@@ -1,48 +1,36 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { Products } from '../../models/products';
 import { ProductServiceService } from '../../services/product-service.service';
+import { FormComponent } from '../../shared/components/form/form.component';
 
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormComponent],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent {
-
   productService = inject(ProductServiceService)
   router = inject(Router)
 
+  onSubmit(product: Products) {
 
-  form = new FormGroup({
-    title: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required, 
-    })
-  })
+    const title = product.title
 
-  onSubmit() {
-
-    const title = this.form.controls.title.value?.trim()
-
-    if(!title){
+    if (!title) {
       this.productService.showMessage('Título é obrigatório', true);
       return
     }
 
     this.productService.createProduct({ title }).subscribe({
       next: () => {
-          this.productService.showMessage("Produto criado com sucesso", false);
-          this.form.reset();
-          setTimeout(() => {
-            this.router.navigate(['']);
-          }, 1000);
+        this.productService.showMessage("Produto criado com sucesso", false);
+        setTimeout(() => {
+          this.router.navigate(['']);
+        }, 1000);
       },
       error: (error) => {
         this.productService.showMessage("Erro ao criar produto", true);
